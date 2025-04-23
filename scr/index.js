9d182c54035ad4bb85e4e9ad8ab05b8b9d897c15
bugajwiktoria <151373799+bugajwiktoria@users.noreply.github.com>
2025-04-23 18:14:15 +0200

    inject  the forecast HTML from js

--- scr/index.js
+++ scr/index.js
@@ -2,14 +2,20 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = `${response.data.condition.description}`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  humidityElement.innerHTML = response.data.temperature.humidity;
 }
 
 function searchCity(city) {
   let apiKey = "81a0bfo67ab947e3772ct00b30d89de4";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&`;
+
   axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -19,7 +25,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHTML = "";
+
+  days.forEach(function (day) {
+    forecastHTML += `
+      <div class="weather-day">
+        <div class="weather-date">${day}</div>
+        <div class="weather-icon">🌤️</div>
+        <div class="weather-temp">
+          <div class="weather-actual-temp">
+            <strong>18°</strong>
+          </div>
+          9°
+        </div>
+      </div>
+    `;
+  });
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Lisbon");
+displayForecast();
